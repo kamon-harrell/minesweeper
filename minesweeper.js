51f4c26851812c3b2c71096d31cf58e9413ffb21
Kamon <Harrellkamon@gmail.com>
2016-06-21 15:34:47 +1200

    added flagging

--- minesweeper.js
+++ minesweeper.js
@@ -2,16 +2,22 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 function startGame () {
   var boardCells = (document.getElementsByClassName('board')[0].children)
-    for (var i = 0; i < boardCells.length; i++) {
-      addListeners(boardCells[i])
-    }
+  for (var i = 0; i < boardCells.length; i++) {
+    addListeners(boardCells[i])
+  }
 }
 
 function addListeners (element) {
-  element.addEventListener('click',showCell)
+  element.addEventListener('click', showCell)
+  element.addEventListener('contextmenu', markCell)
 }
-
 
 function showCell (evt) {
   evt.target.classList.remove('hidden')
+}
+
+function markCell (evt) {
+  evt.preventDefault()
+  evt.target.classList.toggle('marked')
+  evt.target.classList.toggle('hidden')
 }
