5b5e8b795fa761f7241b84c427165c8e9ca1b57e
Kamon <Harrellkamon@gmail.com>
2016-06-22 22:28:12 +1200

    confused will push on tommorow

--- minesweeper.js
+++ minesweeper.js
@@ -8,7 +8,32 @@ function startGame () {
   var boardCells = (document.getElementsByClassName('board')[0].children)
   for (var i = 0; i < boardCells.length; i++) {
     addListeners(boardCells[i])
+    addCellToBoard(boardCells[i])
   }
+
+  for (var j = 0; j < board.cells.length; j++) {
+    board.cells[j].surroundingMines = countSurroundingMines(board.cells[j])
+  }
+}
+
+// check the number of mines around the clicked cell
+function countSurroundingMines (cell) {
+  var surroundingCells = getSurroundingCells(cell.row, cell.col)
+  var count = 0
+  for (var i = 0; i < surroundingCells.length; i++) {
+    if (surroundingCells[i].isMine) {
+      count++
+    }
+  }
+}
+
+function getSurroundingCells (row, col) {
+  var columns = getRange(getLowerBound(col), getUpperBound(col))
+  var rows = getRange(getLowerBound(row), getUpperBound(row))
+  return board.cells
+    .filter(function (cell) {
+      return columns.includes(cell.col) && rows.includes(cell.row)
+    })
 }
 
 //  this listens to the functions below and dictates how to execute their behaviours
@@ -37,4 +62,24 @@ function getRow (element) {
       return Number(classy[i].split('-')[1])
     }
   }
+}
+
+//  classy and sassy
+function getCol (element) {
+  var classy = element.classList
+  for (var i = 0; i < classy.length; i++) {
+    if (classy[i].substring(0, 3) === 'col') {
+      return Number(classy[i].split('-')[1])
+    }
+  }
+}
+
+//  adds info to board
+function addCellToBoard (element) {
+  var newCell = {
+    row: getRow(element),
+    col: getCol(element),
+    isMine: element.classList.contains('mine')
+  }
+  board.cells.push(newCell)
 }
